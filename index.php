a9acce6a6f5320e54238666253573f620ba00b62
Ilya Zelenko <iliyazelenkog@gmail.com>
2018-10-21 09:42:19 +0300

    added products with sum price

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-
+// https://laravel.com/docs/5.7/collections
 require './vendor/autoload.php';
 
-echo 'Hello world!';
-
-$collection = collect([
+$products = [
     [
-        'name' => 'Ilya',
-        'age' => 18
+        'name' => 'cola',
+        'price' => 20
     ],
     [
-        'name' => 'Andrey',
-        'age' => 20
+        'name' => 'chips',
+        'price' => 30
     ]
+];
+
+$sumPrice = collect($products)->sum('price');
+
+dump([
+    'sum' => $sumPrice
 ]);
-
-$collection = $collection->sortByDesc('age')->groupBy('name');
-
-dump($collection->toArray());
